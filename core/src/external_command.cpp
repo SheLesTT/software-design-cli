ff68e0d911001bfe6ba0167aea01b2598ae0be7a
HackAss2002 <HackAss2002@inbox.ru>
2025-11-10 00:18:04 +0300

    add external command

---
+++ core/src/external_command.cpp
@@ -0,0 +1,83 @@
+#include <cstdlib>
+#include <external_command.hpp>
+
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <array>
+#include <stdexcept>
+#include <thread>
+#include <vector>
+
+namespace coreutils {
+
+int ExternalCommand::run(Input& in, Output& out) {
+  std::array<int, 2> writePipe{};
+  if (pipe(writePipe.data()) == -1) {
+    throw std::runtime_error("Pipe went wrong");
+  }
+
+  std::array<int, 2> readPipe{};
+  if (pipe(readPipe.data()) == -1) {
+    throw std::runtime_error("Pipe went wrong");
+  }
+
+  pid_t pid = fork();
+  if (pid < 0) {
+    throw std::runtime_error("Fork went wrong");
+  }
+
+  if (pid == 0) {
+    close(readPipe[0]);
+    close(writePipe[1]);
+    int wPipe = readPipe[1];
+    int rPipe = writePipe[0];
+    dup2(rPipe, STDIN_FILENO);
+    dup2(wPipe, STDOUT_FILENO);
+    close(rPipe);
+    close(wPipe);
+    std::vector<char*> argvs;
+    argvs.reserve(args_.size() + 2);
+    argvs.push_back(command_.data());
+    for (auto& arg : args_) {
+      argvs.push_back(arg.data());
+    }
+    argvs.push_back(nullptr);
+    execvp(command_.data(), argvs.data());
+    __builtin_unreachable();
+  } else {
+    close(writePipe[0]);
+    close(readPipe[1]);
+    int wPipe = writePipe[1];
+    int rPipe = readPipe[0];
+    child_ = pid;
+    std::thread writer([&]() {
+      while (true) {
+        auto data = in.read(DEFAULT_BLOCK_SIZE);
+        if (data.empty()) {
+          break;
+        }
+        write(wPipe, data.data(), data.size());
+      }
+      close(wPipe);
+    });
+    std::thread reader([&]() {
+      std::array<char, DEFAULT_BLOCK_SIZE> buf{};
+      while (true) {
+        ssize_t size = read(rPipe, buf.data(), DEFAULT_BLOCK_SIZE);
+        if (size == 0) {
+          break;
+        }
+        out.write({buf.data(), buf.data() + size});
+      }
+      close(rPipe);
+    });
+    writer.join();
+    reader.join();
+    int status = 0;
+    auto w = waitpid(pid, &status, WUNTRACED);
+    return WEXITSTATUS(status);
+  }
+}
+
+}  // namespace coreutils
---
+++ test/external_command_test.cpp
@@ -0,0 +1,39 @@
+#include <external_command.hpp>
+
+#include <dummy_input.hpp>
+#include <dummy_output.hpp>
+#include <pipe.hpp>
+#include <text_output.hpp>
+
+#include <gtest/gtest.h>
+
+namespace coreutils::test {
+
+TEST(ExternalCommand, Basic) {
+  ExternalCommand cmd("echo", {"Test", "External Command"});
+  DummyInput in;
+  TextOutput out;
+  ASSERT_EQ(cmd.run(in, out), 0);
+  ASSERT_EQ(out.read(), "Test External Command\n");
+}
+
+TEST(ExternalCommand, ProcessInteraction) {
+  ExternalCommand cat("cat", {"../../test/data/file.txt"});
+  ExternalCommand wc("wc", {});
+  DummyInput in;
+  auto [pipeIn, pipeOut] = createPipe();
+  TextOutput out;
+  ASSERT_EQ(cat.run(in, *pipeOut), 0);
+  pipeOut.reset();
+  ASSERT_EQ(wc.run(*pipeIn, out), 0);
+  ASSERT_EQ(out.read(), "     15      98    1039\n");
+}
+
+TEST(ExternalCommand, Error) {
+  ExternalCommand cmd("cat", {"not_existing_file_path"});
+  DummyInput in;
+  DummyOutput out;
+  ASSERT_EQ(cmd.run(in, out), 1);
+}
+
+}  // namespace coreutils::test
